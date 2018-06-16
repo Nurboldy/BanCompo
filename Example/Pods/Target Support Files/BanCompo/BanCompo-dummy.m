6f734d8d7c991f1c7cba744f652c8d490212e83a
Nurboldy <Bnurboldy@gmail.com>
2018-06-16 18:48:44 +0800

    First pod lib

---
+++ Example/Pods/Target Support Files/BanCompo/BanCompo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BanCompo : NSObject
+@end
+@implementation PodsDummy_BanCompo
+@end
---
+++ Example/Pods/Target Support Files/Pods-BanCompo_Tests/Pods-BanCompo_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BanCompo_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BanCompo_Tests
+@end
